aa40ba379dc20b2fa4d282de884b98ac97677e63
lgmro <marinholgabriel@gmail.com>
2023-11-10 20:09:15 -0300

    Create test to deleteSector on SetorController

--- src/test/java/com/gestaoequalidade/testsImplementation/setor/controller/SetorControllerTest.java
+++ src/test/java/com/gestaoequalidade/testsImplementation/setor/controller/SetorControllerTest.java
@@ -1,9 +1,9 @@
-package com.gestaoequalidade.testsImplementation.controller;
+package com.gestaoequalidade.testsImplementation.setor.controller;
 
 import com.gestaoequalidade.testsImplementation.setor.component.UserInputProvider;
-import com.gestaoequalidade.testsImplementation.setor.controller.SetorController;
 import com.gestaoequalidade.testsImplementation.setor.entity.Setor;
 import com.gestaoequalidade.testsImplementation.setor.service.SetorService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
@@ -35,6 +35,15 @@ public class SetorControllerTest {
 
     @Captor
     private ArgumentCaptor<Long> idSetorCaptor;
+
+    private Setor setor = new Setor();
+
+    @BeforeEach
+    void setUp() {
+        setor.setId(1L);
+        setor.setNome(SETOR_NAME_RH);
+
+    }
 
     @Test
     void dadoQueExisteSetoresCadastradosQuandoChamarListarSetoresDeveRetornarAListaDeSetores() {
@@ -82,11 +91,6 @@ public class SetorControllerTest {
 
     @Test
     void dadoQueUsuarioQuerEditarUmSetorQuandoChamarEditarSetorEUsuarioConfirmarAlteracaoDeveAtualizarSetorCorretamente() {
-        // Arrange
-        Setor setor = new Setor();
-        setor.setId(1L);
-        setor.setNome(SETOR_NAME_RH);
-
         // Mock
         when(userInputProvider.getUserInput()).thenReturn("1", SETOR_NAME_FINANCEIRO, RESPOSTA_SIM);
         when(setorService.buscarSetorPorId(setor.getId())).thenReturn(setor);
@@ -100,11 +104,11 @@ public class SetorControllerTest {
         verify(userInputProvider, times(2)).setUserOutput(DIVISOR);
         verify(userInputProvider, times(1)).setUserOutput(CONFIRMACAO_ALTERACAO_NOME_SETOR_MESSAGEM);
         verify(userInputProvider, times(1)).setUserOutput(CONFIRMACAO_NOVO_NOME_SETOR_MESSAGEM);
-        verify(userInputProvider, times(1)).setUserOutput(OPCOES_CONFIRMACAO_ALTERACAO_NOME_SETOR_MESSAGEM);
+        verify(userInputProvider, times(1)).setUserOutput(OPCOES_CONFIRMACAO_ALTERACAO_EXLUSAO_SETOR_MESSAGEM);
         verify(userInputProvider, times(1)).setUserOutput(setor.getId() + ". " + setor.getNome());
         verify(userInputProvider, times(1)).setUserOutput(setor.getId() + ". " + SETOR_NAME_FINANCEIRO);
         verify(setorService, times(1)).atualizarSetor(idSetorCaptor.capture(), setorCaptor.capture());
-        verify(userInputProvider, times(1)).setUserOutput(ALTERACAO_NOME_SETOR_BEM_SUCESSIDA_MESSAGEM);
+        verify(userInputProvider, times(1)).setUserOutput(ALTERACAO_NOME_SETOR_BEM_SUCEDIDA_MESSAGEM);
 
 
         // Assert
@@ -115,11 +119,6 @@ public class SetorControllerTest {
 
     @Test
     void dadoQueUsuarioQuerEditarUmSetorQuandoChamarEditarSetorEUsuarioCancelarAlteracaoDeveInterromperAlteracao() {
-        // Arrange
-        Setor setor = new Setor();
-        setor.setId(1L);
-        setor.setNome(SETOR_NAME_RH);
-
         // Mock
         when(userInputProvider.getUserInput()).thenReturn("1", SETOR_NAME_FINANCEIRO, RESPOSTA_NAO);
         when(setorService.buscarSetorPorId(setor.getId())).thenReturn(setor);
@@ -133,7 +132,7 @@ public class SetorControllerTest {
         verify(userInputProvider, times(2)).setUserOutput(DIVISOR);
         verify(userInputProvider, times(1)).setUserOutput(CONFIRMACAO_ALTERACAO_NOME_SETOR_MESSAGEM);
         verify(userInputProvider, times(1)).setUserOutput(CONFIRMACAO_NOVO_NOME_SETOR_MESSAGEM);
-        verify(userInputProvider, times(1)).setUserOutput(OPCOES_CONFIRMACAO_ALTERACAO_NOME_SETOR_MESSAGEM);
+        verify(userInputProvider, times(1)).setUserOutput(OPCOES_CONFIRMACAO_ALTERACAO_EXLUSAO_SETOR_MESSAGEM);
         verify(userInputProvider, times(1)).setUserOutput(setor.getId() + ". " + setor.getNome());
         verify(userInputProvider, times(1)).setUserOutput(setor.getId() + ". " + SETOR_NAME_FINANCEIRO);
         verify(userInputProvider, times(1)).setUserOutput(ALTERACAO_NOME_SETOR_CANCELADA_MESSAGEM);
@@ -149,6 +148,59 @@ public class SetorControllerTest {
         setorController.editarSetor();
 
         // Verify Mocks
+        verify(userInputProvider, times(1)).setUserOutput(ID_NOVO_SETOR_MESSAGEM);
+        verify(userInputProvider, times(1)).setUserOutput(SETOR_NAO_ENCONTRADO_MESSAGEM);
+    }
+
+    @Test
+    void dadoQueUsuarioQuerExcluirUmSetorQuandoUsuarioConfirmarAExclusaoDeveExcluirSetorApropriadamente() {
+        // Mock
+        when(userInputProvider.getUserInput()).thenReturn("1", RESPOSTA_SIM);
+        when(setorService.buscarSetorPorId(setor.getId())).thenReturn(setor);
+
+        // Act
+        setorController.excluirSetor();
+
+        // Verify Mocks
+        verify(userInputProvider, times(1)).setUserOutput(ID_SETOR_A_EXCLUIR_MESSAGEM);
+        verify(userInputProvider, times(1)).setUserOutput(CONFIRMACAO_EXCLUSAO_SETOR_MESSAGEM);
+        verify(userInputProvider, times(1)).setUserOutput(OPCOES_CONFIRMACAO_ALTERACAO_EXLUSAO_SETOR_MESSAGEM);
+        verify(setorService, times(1)).excluirSetor(idSetorCaptor.capture());
+        verify(userInputProvider, times(1)).setUserOutput(EXCLUSAO_SETOR_BEM_SUCEDIDA_MESSAGEM);
+
+        // Assert
+        assertEquals(setor.getId(), idSetorCaptor.getValue());
+    }
+
+
+    @Test
+    void dadoQueUsuarioQuerExcluirUmSetorQuandoUsuarioCancelarAExclusaoDeveMostrarMessagemDeExlusaoCancelada() {
+        // Mock
+        when(userInputProvider.getUserInput()).thenReturn("1", RESPOSTA_NAO);
+        when(setorService.buscarSetorPorId(setor.getId())).thenReturn(setor);
+
+        // Act
+        setorController.excluirSetor();
+
+        // Verify Mocks
+        verify(userInputProvider, times(1)).setUserOutput(ID_SETOR_A_EXCLUIR_MESSAGEM);
+        verify(userInputProvider, times(1)).setUserOutput(CONFIRMACAO_EXCLUSAO_SETOR_MESSAGEM);
+        verify(userInputProvider, times(1)).setUserOutput(OPCOES_CONFIRMACAO_ALTERACAO_EXLUSAO_SETOR_MESSAGEM);
+        verify(userInputProvider, times(1)).setUserOutput(EXCLUSAO_SETOR_CANCELADA_MESSAGEM);
+    }
+
+
+    @Test
+    void dadoQueUsuarioQuerExcluirUmSetorQuandoSetorDesejadoNaoForEncontradoDeveExibirUmaMessagemDeSetorNaoEncontrado() {
+        // Mock
+        when(userInputProvider.getUserInput()).thenReturn("0");
+        when(setorService.buscarSetorPorId(0L)).thenReturn(null);
+
+        // Act
+        setorController.excluirSetor();
+
+        // Verify Mocks
+        verify(userInputProvider, times(1)).setUserOutput(ID_SETOR_A_EXCLUIR_MESSAGEM);
         verify(userInputProvider, times(1)).setUserOutput(SETOR_NAO_ENCONTRADO_MESSAGEM);
     }
 
@@ -162,10 +214,14 @@ public class SetorControllerTest {
     private static final String NOME_NOVO_SETOR_MESSAGEM = "Novo nome do setor:";
     private static final String CONFIRMACAO_ALTERACAO_NOME_SETOR_MESSAGEM = "Você tem certeza que quer alterar o Setor de:";
     private static final String CONFIRMACAO_NOVO_NOME_SETOR_MESSAGEM = "Para:";
-    private static final String OPCOES_CONFIRMACAO_ALTERACAO_NOME_SETOR_MESSAGEM = "S/N?";
+    private static final String OPCOES_CONFIRMACAO_ALTERACAO_EXLUSAO_SETOR_MESSAGEM = "S/N?";
     private static final String ALTERACAO_NOME_SETOR_CANCELADA_MESSAGEM = "Atualização do setor foi cancelada.";
-    private static final String ALTERACAO_NOME_SETOR_BEM_SUCESSIDA_MESSAGEM = "Setor atualizado com sucesso!";
+    private static final String ALTERACAO_NOME_SETOR_BEM_SUCEDIDA_MESSAGEM = "Setor atualizado com sucesso!";
     private static final String SETOR_NAO_ENCONTRADO_MESSAGEM = "Setor não encontrado.";
+    private static final String ID_SETOR_A_EXCLUIR_MESSAGEM = "ID do setor a ser excluído:";
+    private static final String CONFIRMACAO_EXCLUSAO_SETOR_MESSAGEM = "Você tem certeza que quer excluir o Setor:";
+    private static final String EXCLUSAO_SETOR_BEM_SUCEDIDA_MESSAGEM = "Setor excluído com sucesso!";
+    private static final String EXCLUSAO_SETOR_CANCELADA_MESSAGEM = "Exclusão do setor foi cancelada.";
     private static final String RESPOSTA_SIM = "S";
-    private static final String RESPOSTA_NAO = "N";
+    private static final String RESPOSTA_NAO = "n";
 }
