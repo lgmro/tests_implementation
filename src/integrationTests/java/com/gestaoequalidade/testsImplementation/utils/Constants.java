2d04d3411c2621f6bdb71347660aa424602a78e0
lgmro <marinholgabriel@gmail.com>
2023-11-17 20:01:20 -0300

    Creating integration tests and task to run tests on gradle

---
+++ src/integrationTests/java/com/gestaoequalidade/testsImplementation/utils/Constants.java
@@ -0,0 +1,9 @@
+package com.gestaoequalidade.testsImplementation.utils;
+
+public class Constants {
+    public static final String SETOR_RH_NAME = "RH";
+    public static final String SETOR_FINANCEIRO_NAME = "FINANCEIRO";
+    public static final String[] OPCOES = { "0", "1", "2", "3" };
+
+    public static final String RESPOSTA_SIM = "S";
+}
